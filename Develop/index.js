bdd691c4d383c0c9ac9e75beb148314b97b28396
Badr Almadi <badr.almadhi95@gmail.com>
2023-04-16 00:51:28 -0600

    creating array questions

---
+++ Develop/index.js
@@ -0,0 +1,28 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?' (required),
+        validate: titleInput => {
+if (titleInput) {
+    return true;
+} else {
+    console.log('Please enter your title!')
+}
+        }
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
